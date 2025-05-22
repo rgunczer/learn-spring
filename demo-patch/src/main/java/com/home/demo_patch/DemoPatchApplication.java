940327a1dd37ce0480d1c4b4bece53653ed6ffb6
Robert Gunczer <rgunczer@gmail.com>
2025-05-22 06:34:49 +0200

    add skeleton for patch sample project

---
+++ demo-patch/src/main/java/com/home/demo_patch/DemoPatchApplication.java
@@ -0,0 +1,13 @@
+package com.home.demo_patch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPatchApplication.class, args);
+	}
+
+}
