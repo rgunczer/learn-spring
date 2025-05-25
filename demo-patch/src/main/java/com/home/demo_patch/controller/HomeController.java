cf6cadb8731a0d417a79c2323b438040207be0f2
Robert Gunczer <rgunczer@gmail.com>
2025-05-25 02:29:42 +0200

    add home controller

---
+++ demo-patch/src/main/java/com/home/demo_patch/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.home.demo_patch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/home")
+@RestController
+public class HomeController {
+
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+
+}
